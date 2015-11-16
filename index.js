46b2b9b1bcf66016dbbf0c69d913a30735afefc6
Garcia <mgarcia@atlanta.nextgen.com>
2015-11-16 16:02:22 -0500

    successfully connected to SQL Server.  Learning how to query JSON

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var app = express();
-var portNumber = '12345'
+var portNumber = '8000';
 var path = require('path');
 var rootPath = path.normalize(__dirname);
 var mongoClient = require('mongodb');
-var mongoConnStr = 'mongodb://localhost:27017/ngprod';
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-var mongoClient = require('mongodb');
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-var mongoConnStr = 'mongodb://localhost:27017/ngdevl'
+var mongoConnStr = 'mongodb://localhost:27017/ngdevl';
 var sql = require('mssql');
 var sqlConfig = {
     user: 'sa',
@@ -18,7 +14,7 @@ var sqlConfig = {
     server: 'localhost',
     database: 'NG58GR_83'
 }
-
+var jsonQuery = require('json-query');
 
 app.use(express.static(rootPath + '/app'));
 
@@ -50,39 +46,15 @@ app.get('/viewPerson', function(req, res){
         var request = new sql.Request(sqlConnection);
         request.query('select top 10 * from person', function(err, recordset){
             //console.dir(JSON.stringify(recordset));
-            res.send(JSON.stringify(recordset));
+            //res.send(JSON.stringify(recordset));
+            var data = 'person:' + JSON.stringify(recordset);
+            //console.log(jsonQuery('data[person_id=5235EABA-0003-48A6-A949-8850C5014C72].*', {data: data}));
+            //console.log(data);
+            //console.log(jsonQuery('data[person_id=5235EABA-0003-48A6-A949-8850C5014C72].*', {data: data}));
+            res.send('data[person_id=5235EABA-0003-48A6-A949-8850C5014C72].last_name', {data: data});
         })
     });
 });
 
-
-
-
 app.listen(portNumber);
 console.log('listening on port ' + portNumber);
-
-
-/*
-    Pre-cooked.
-
-    var mongoClient = require('mongodb');
-    var mongoConnStr = 'mongodb://localhost:27017/ngprod';
-    var bodyParser = require('body-parser');
-    var jsonParser = bodyParser.json();
-
-     app.post('/savePatient', jsonParser, function(req,res){
-
-     mongoClient.connect(mongoConnStr, function(err, db){
-     if(err){
-     console.log('failed to connect to mongo');
-     }
-     else{
-     console.log('successfully connected to mongo');
-     }
-     db.close();
-     })
-
-     });
-
-
- */
